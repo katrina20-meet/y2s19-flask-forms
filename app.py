eff385dff36ee777df140be6d3763e198addd4c5
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:30:25 -0400

    flask delete add

--- app.py
+++ app.py
@@ -12,5 +12,23 @@ def display_student(student_id):
 
 #Create an '/add' route here:
 
+@app.route('/add', methods=['GET', 'POST'])
+def add_student_route():
+	if request.method == 'GET':
+		return render_template('add.html')
+	else:
+		name = request.form['student_name']
+		year = request.form['student_year']
+		
+		add_student(name, year)
+		print("received POST request!!!!")
+		return render_template('add.html', n= name, y=year)
+
+@app.route('/delete/<int:student_id>', methods=['POST'])
+def delete_student_id(student_id):
+	redirect(url_for('home.html'))
+
+
+		
 if __name__ == '__main__':
     app.run(debug=True)
